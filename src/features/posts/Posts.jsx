6adbc0c3b6bc7dab455525297e9d595294efa215
HaruGrossman <dogtaboodog@yahoo.com>
2023-10-27 14:19:45 -0500

    Correct typo

--- src/features/posts/Posts.jsx
+++ src/features/posts/Posts.jsx
@@ -1,33 +1,21 @@
-// fetches an array of posts from the API and renders each post as a <Post>. This is done with useState, useEffect, and fetch
-
-//since we are using useEffect and useState they need to be imported first
-import { useEffect, useState } from "react";
-//import the single post 
+//because we created out store and api else where we can refactor this file
 import Post from "./Post";
+//import out query we made in store
+import { useGetPostsQuery } from "./postSlice";
 
 //write out component 
 function Posts() {
-    const [posts, setPosts] = useState([]); //set up useState
+    const { data: posts } = useGetPostsQuery();
 
-    //fetch activities from the API when the component mounts
-    useEffect(() => {
-        const fetchPosts = async () => { //setting up out api pull location info /async because we are waiting for out request to be answered
-            const COHORT = "2309-FSA-ET-WEB-FT-SF";
-            const API_URL = `https://strangers-things.herokuapp.com/api/${COHORT}`; //using `` so we can use ${}
-            response = await fetch(`${API_URL}/posts`);
-            json = await response.json();
-
-            setPosts(json.data.posts); // we are filling in out useState setPosts with the fetched extracted array of posts
-        };
-        fetchPosts();// call our function
-    }, []); //empty endpoint array
-    return ( //closing our Posts function using the array of posts we got from fetchPosts and mapping over them
+    return (
         <ul>
             {posts.map((post) => (
-                <Post Key={post._id} post={post} />
+                { posts?.map((post) => ( //if there is something of posts to map, then map
+                    <Post key={post._id} post={post} />
+                ))}
             ))}
         </ul>
-    )
+    );
 }
 
 
