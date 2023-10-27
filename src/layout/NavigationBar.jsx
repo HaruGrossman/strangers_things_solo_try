fd96b3c7737c06da77827172e5a1ea5e61ce2b76
HaruGrossman <dogtaboodog@yahoo.com>
2023-10-27 15:48:16 -0500

    Create and add Register

---
+++ src/layout/NavigationBar.jsx
@@ -0,0 +1,18 @@
+//create a navigationBar component with a single link to /posts. This should be rendered inside <Root>
+
+import { Link } from "react-router-dom";
+
+function NavigationBar() {
+    return (
+        <nav>
+            <h1>{"Stranger Things Solo Try"}</h1>
+            <menu>
+                <li>
+                    <Link to="/posts">Posts</Link>
+                </li>
+            </menu>
+        </nav>
+    );
+}
+
+export default NavigationBar // now add this to the Root.jsx file
--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,16 @@
 //create a <Root> component with an <Outlet>. This will serve as the main template from the app. Refactor the router in main.jsx so that the existing routes become the children of <Root>. 
 import { Outlet } from "react-router-dom";
+import NavigationBar from "./NavigationBar";
+//after creating NavigationBar.jsx import and add to Root function
 
 function Root() {
     return (
-        <main>
-            <Outlet />
-        </main>
+        <>
+            <NavigationBar />
+            <main>
+                <Outlet />
+            </main>
+        </>
     )
 }
 
