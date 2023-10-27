00bcc08ae134613c10bb1532a017f095393b0d86
HaruGrossman <dogtaboodog@yahoo.com>
2023-10-27 15:10:31 -0500

    Add link for PostDetails in Post.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
 //import the store, and provider
 import store from './store/store.js'
 import { Provider } from 'react-redux'
 
+//create a router in main.jsx with "/", "/posts" rendering "<PostPage>"
+//"/posts/:id" render <PostPage>
+// this will make it unnessary to have the App.jsx
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PostDetails from "./features/posts/PostDetails";
+import PostPage from "./features/posts/PostPage";
+
+const router = createBrowserRouter([
+  { path: "/" || "/posts", element: <PostPage /> }
+  { path: "/posts/:id", element: <PostDetails /> }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}> {/* provider with telling store is {store} around the App */}
-      <App />
-    </Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
