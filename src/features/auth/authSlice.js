4a7c5be17653beb40b51fdf10235c611d42b0721
HaruGrossman <dogtaboodog@yahoo.com>
2023-10-28 11:30:18 -0500

    Update Register.jsx and Login.jsx for sending credentials

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -2,6 +2,8 @@
 
 //import out slice headquarters api.jsx
 import api from "../../store/api"
+import { createSlice } from "@reduxjs/toolkit";//create a slice to store the JWT
+
 
 const authApi = api.injectEndpoints({
     endpoints: (builder) => ({
@@ -23,3 +25,45 @@ const authApi = api.injectEndpoints({
 })
 
 export const { useRegisterMutation, useLoginMutation } = authApi; // send out our deconstructed items
+
+
+//the initial state should grab the token from the sessionStorage
+//define extraReducers so that when wither mutation is fulfilled, the token is grabbed from the payload and stored in both state and sessionStorage
+//export a selector to select the token
+//connect the slice to the store
+
+//this key is used to store the token in the browser's sessionStorage
+const TOKEN_KEY = "TOKEN";
+
+//this function is used only as an extraReducer to store the token in states and session storage
+// @param state - the current state of the slice
+// @param payload - the response sent back from the API
+const storeToken = (state, { payload }) => {
+    //the documentation tells us how to access the token from the payload
+    state.token = payload.data.token;
+    sessionStorage.setItem(TOKEN_KEY, payload.data.token);
+};
+//we use a slice to keep track of the token
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        token: sessionStorage.getItem(TOKEN_KEY),
+    },
+    //add a "logout" reducer to the authSlice; when dispatched, the "logout" action should set the token in state to "null" and remove the token from sessionStorage
+    reducers: {
+        logout: (state) => {
+            state.token = null;
+            sessionStorage.removeItem(TOKEN_KEY);
+        }
+    }
+
+    extraReducers: (builder) => {
+        builder.addMatcher(api.endpoints.register.matchFulfilled, storeToken);
+        builder.addMatcher(api.endpoints.login.matchFulfilled, storeToken);
+    },
+});
+
+//the 'auth' key here should match the name of the authSlice
+export const selectToken = (state) => state.auth.token;
+
+export default authSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import api from "./api"; //from api.js
 import { curryGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
+import authReducer from "../features/auth/authSlice"; // after adding auth in authSlice
+
 
 const store = configureStore({ //configuring our store for export
     reducer: {
         [api.reducerPath]: api.reducer,
+        auth: authReducer//the additional "auth" key here should batch the name of the authSlice
     },
     middleware: (curryGetDefaultMiddleware) =>
         curryGetDefaultMiddleware().concat(api.middleware),
